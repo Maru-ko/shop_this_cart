e6070db3e66ece496fa74b82bba044c31cb825e1
Marcos <53981643+Maru-ko@users.noreply.github.com>
2022-09-19 13:01:56 -0400

    Issue 1

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,9 @@
-index.js
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return React.createElement("div", null, "Hello World!")
+}
+
+const root = document.getElementById("root");
+ReactDOM.createRoot(root).render(App());
