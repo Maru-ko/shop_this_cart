918a71a7e04e41863540772c81d93bbb5a0bd76a
Marcos <53981643+Maru-ko@users.noreply.github.com>
2022-09-21 13:38:42 -0400

    Can add product and round price number

--- client/src/components/AddForm.js
+++ client/src/components/AddForm.js
@@ -14,13 +14,27 @@
 //   element2.style.display = "none";
 // }
 import { useState } from "react";
-
-const AddForm = () => {
+// title price quantity
+const AddForm = ({ onSubmit }) => {
   const [showForm, setShowForm] = useState(false);
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [quantity, setQuantity] = useState("");
 
   const toggleForm = () => {
     setShowForm(!showForm);
   }
+
+  const handleAddButton = (e) => {
+    e.preventDefault();
+    const priceInt = parseFloat(price).toFixed(2);
+    const quantityInt = parseInt(quantity);
+    const newProduct = {
+      title, price:priceInt, quantity:quantityInt,
+    }
+    onSubmit(newProduct);
+  }
+
   return (
     <div className = { showForm ? "add-form visible" : "add-form" }>
       <p>
@@ -30,21 +44,27 @@ const AddForm = () => {
       <form>
         <div className="input-group">
           <label for="product-name">Product Name</label>
-          <input type="text" id="product-name" value=""/>
+          <input type="text" id="product-name" value={ title } onChange={(event) => {
+            setTitle(event.target.value);
+          } }/>
         </div>
 
         <div className="input-group">
           <label for="product-price">Price</label>
-          <input type="text" id="product-price" value=""/>
+          <input type="text" id="product-price" value={ price } onChange={(event) => {
+            setPrice(event.target.value);
+          }}/>
         </div>
 
         <div className="input-group">
           <label for="product-quantity">Quantity</label>
-          <input type="text" id="product-quantity" value=""/>
+          <input type="text" id="product-quantity" value={ quantity } onChange={(event) => {
+            setQuantity(event.target.value);
+          }}/>
         </div>
 
         <div className="actions form-actions">
-          <a className="button">Add</a>
+          <a className="button" onClick={ handleAddButton }>Add</a>
           <a className="button" onClick={toggleForm}>Cancel</a>
         </div>
       </form>
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,3 +1,6 @@
+const axios = require("axios");
+// const { set } = require("mongoose");
+
 const { useState, useEffect } = require("react");
 let rawData = require("../../mockData/data");
 rawData = rawData.default;
@@ -7,12 +10,21 @@ const Products = require("./Products");
 const Main = () => {
   const [products, setProducts] = useState([]);
   useEffect(() => {
-    setProducts(rawData);
+    // setProducts(rawData);
+    const getProducts = async () => {
+      const initialProducts = await axios.get("/api/products");
+      setProducts(initialProducts.data)
+    }
+    getProducts();
   }, []);
+  const handleAddProduct = async (newProduct) => {
+    const returnedProduct = await axios.post("/api/products", newProduct);
+    setProducts(products.concat(returnedProduct.data));
+  }
   return (
     <main>
       <Products products={products} />
-      <AddForm />
+      <AddForm onSubmit={handleAddProduct} />
     </main>
   )
 }
