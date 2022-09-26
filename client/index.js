36219582fdacfec23c1a5998a0d21a33d35243b7
Mohamad EL-Chanti <m.elchanti@gmail.com>
2022-09-26 18:39:16 -0400

    docker files

--- client/index.js
+++ client/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./src/components/App";
+import 'regenerator-runtime/runtime';
 //
 
 const root = document.getElementById("root");
